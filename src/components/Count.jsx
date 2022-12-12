0365e2847cd9041c570180db2b3702314528055c
Davi Marcilio <davimarcilio.js@gmail.com>
2022-12-12 09:02:54 -0300

    testes

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 export default function Count(props) {
     return (
-        <div>
-            <p >{props.countname}<br /> <br /><b className='font-normal text-7xl text-[#4D4C59]'>{props.number}</b></p>
+        <div className='flex flex-col justify-center items-center'>
+            <p>{props.countname}</p>
+            <p > <br /><b className='font-normal text-7xl text-[#4D4C59]'>{props.number}</b></p>
         </div>
     )
 }
