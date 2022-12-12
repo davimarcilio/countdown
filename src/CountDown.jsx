965154a5961f35f43162f1f615897a37b4bd7679
Davi Marcilio <davimarcilio.js@gmail.com>
2022-12-12 10:18:06 -0300

    Await a minutes

--- src/CountDown.jsx
+++ src/CountDown.jsx
@@ -11,13 +11,47 @@ import moment from 'moment';
 import { useEffect, useState } from 'react';
 import rocketLoading from './assets/images/rocketLoading.gif'
 moment().format();
+const mailchimpClient = require("@mailchimp/mailchimp_transactional")(
+  "md-eZxyL92TNDuJ4Xs2U4qwew"
+);
 
 
 export default function CountDown() {
 
+  // async function sendEmailWithRocketInfos(e) {
+
+
+  //   const run = async () => {
+  //     const responseRocket = await axios.get('https://fdo.rocketlaunch.live/json/launches/next/1');
+  //     const data = response.data;
+  //     const response = await mailchimpClient.messages.send({
+  //       message: {
+  //         text: data.result[0].launch_description,
+  //         subject: `Missão ${data.result[0].name}`,
+  //         from_email: 'davimarcilio.js@gmail.com',
+  //         from_name: 'Rocket Launcher Web Site',
+  //         to: 
+
+  //       }
+  //     });
+  //     console.log(response);
+  //     console.log(response);
+  //   };
+
+  //   run();
+
+
+
+  // }
+
+
+
+
+
 
   const [launchState, setLaunchState] = useState(false)
   const [loading, setLoading] = useState(true)
+  const [modal, setModal] = useState(false)
   const dispatch = useDispatch();
   async function nextRocketLaunchDate() {
     const response = await axios.get('https://fdo.rocketlaunch.live/json/launches/next/1');
@@ -45,6 +79,13 @@ export default function CountDown() {
 
   return (
     <div >
+      <div className={`${modal ? '' : 'hidden'} flex-col bg-white absolute w-screen h-screen justify-center items-center flex`}>
+        <h1 className='text-2xl font-bold font-Poppins'>Inscreva-se para receber atualizacoes sobre o proximo foguete a ser lancado.</h1>
+        <form className='flex flex-row'>
+          <input className='bg-purple-900 text-white placeholder:text-white py-2 px-6 w-full' type="email" placeholder='EMAIL:' required />
+          <input type="submit" value={"inscreva-se"} />
+        </form>
+      </div>
       <div className={`absolute w-screen h-screen justify-center items-center flex bg-white ${!loading ? 'hidden' : ''}`}><img className='w-1/3' src={rocketLoading} alt="rocket" /></div>
       <div className='h-screen flex flex-row justify-evenly items-center'>
         <div className='flex flex-col font-Poppins justify-center items-center'>
@@ -69,7 +110,7 @@ export default function CountDown() {
           </div>
           <div className='flex flex-col justify-center items-center w-full mt-11'>
             <p className='text-[#9C9AB6] text-sm'>Inscreva-se para saber mais sobre o lançamento</p>
-            <button className='mt-8 px-8 py-3 bg-[#6C63FF] rounded-xl text-white text-base font-normal'>Inscreva-se</button>
+            <button onClick={() => setModal(true)} className='mt-8 px-8 py-3 bg-[#6C63FF] rounded-xl text-white text-base font-normal'>Inscreva-se</button>
           </div>
         </div>
         <img className='h-fit' src={Rocket} alt="rocket" />
